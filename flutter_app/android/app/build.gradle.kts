5f37a982e121e015bacfe700570fd84bd18dad17
wiredevelop <265056906+wiredevelop@users.noreply.github.com>
2026-04-25 16:45:54 +0000

    chore(android): set package to app.wiredevelop.pt and switch codemagic signing to env vars

--- flutter_app/android/app/build.gradle.kts
+++ flutter_app/android/app/build.gradle.kts
@@ -16,7 +16,7 @@ val hasCodemagicSigning =
     !cmKeyPassword.isNullOrBlank()
 
 android {
-    namespace = "com.wiredevelop.wire_crm_app"
+    namespace = "app.wiredevelop.pt"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.wiredevelop.wire_crm_app"
+        applicationId = "app.wiredevelop.pt"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
